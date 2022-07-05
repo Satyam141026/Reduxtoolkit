2ae986aa970ad783bd9a8915ade018cfa67d6658
“Satyam@141026” <satyam@141026@gmail.com>
2022-07-05 20:30:41 +0530

    async optation

---
+++ src/redux/rootSlice.js
@@ -0,0 +1,40 @@
+import {  createSlice } from '@reduxjs/toolkit';
+
+
+
+const rootSlice=createSlice({
+    name:'mySlice',
+    initialState:{
+      value:'satyam'
+    },
+    reducers:{
+      showData:function(state, action){
+       state.value = action.payload;
+    },
+    showApi:function(state,action){
+     // state.value = action.payload.title;
+      console.log(action.payload.title)
+    }
+    
+    }
+  
+  })
+  
+
+  export const  apiCall=()=>{
+  return (dispatch)=>{
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then((data)=>{
+      dispatch(showApi(data))
+
+    })
+  }
+    
+             
+  }
+
+  export const { showData , showApi} = rootSlice.actions;
+
+
+  export default rootSlice.reducer;
